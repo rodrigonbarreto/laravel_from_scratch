8c249e6ef92b4bda57554d83db62364ed646c41f
Rodrigo Barreto <rodrigo.barreto@mall-connect.com>
2017-11-17 23:24:15 +0100

    setup test and create test for posts ep21 extra with dataprovider

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,13 +2,18 @@
 
 namespace Tests\Feature;
 
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use \App\Post;
 
 class ExampleTest extends TestCase
 {
+
+    // this restore database
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -16,8 +21,45 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/')->assertSee('The Bulma Blog');
 
         $response->assertStatus(200);
+    }
+
+    /**
+     *
+     * @dataProvider  listActionDataProvider
+     * @param array   $expectArchives
+     */
+    public function testArchives($expectArchives)
+    {
+        $first = factory(Post::class)->create();
+        $second = factory(Post::class)->create(['created_at' =>  Carbon::now()->subMonth()]);
+        $posts = Post::archives();
+        $this->assertCount(2, $posts);
+        $this->assertEquals($expectArchives, $posts);
+    }
+
+    /**
+     * @return array
+     */
+    public function listActionDataProvider()
+    {
+        return [
+            'list of archives' => [
+                'data' => [
+                    [
+                        "year" => '2017',
+                        "month" => Carbon::now()->subMonth()->format('F'),
+                        "published" => 1,
+                    ],
+                    [
+                        "year" => '2017',
+                        "month" => Carbon::now()->format('F'),
+                        "published" => 1,
+                    ]
+                ],
+            ],
+        ];
     }
 }
